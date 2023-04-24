d061bb0ef378b62a5db5f4af6fb0b6b98bb1e92e
netoeac <netoeac.dev@gmail.com>
2023-04-24 20:34:55 -0300

    hotfix-image-show

--- src/mock/products.mock.js
+++ src/mock/products.mock.js
@@ -13,7 +13,7 @@ export const PRODUCTS_MOCKS = [
     {
         id: 2,
         imgPath: "produto_2.png",
-        imgPathDetail: "../../public/produto_2.png",
+        imgPathDetail: "../../src/assets/products/produto_2.png",
         title: "Console Xbox Series S 512GB SSD",
         infoProducts: "Para você que é fã de games e está a procura de um novo console para uma nova experiência, precisa conhecer o Xbox Series S da Microsoft na cor branca. Lançado em 2020, ele tem SSD de 512GB para armazenar diversos jogos e 1 controle sem fio que deixa você livre para jogar. Use apenas com jogos digitais. O console não reproduz discos físicos. Inclui o download gratuito dos jogos Fortnite, Rocket League e Fall Guys. O conteúdo é entregue diretamente ao seu console durante a configuração e nenhum código é necessário.",
         price: "R$ 2.516,55",
