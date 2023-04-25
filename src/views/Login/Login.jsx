37f792ddca5b6e117457baa279e82871985c0ef6
netoeac <netoeac.dev@gmail.com>
2023-04-25 10:43:28 -0300

    login validation with sweetalert2

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,31 +1,122 @@
-import { Link } from 'react-router-dom';
-import Logo from '../../assets/images/dnc-logo.png'
+import { Link } from "react-router-dom";
+import Logo from "../../assets/images/dnc-logo.png";
 import "./index.scss";
+import { useState } from "react";
+import Swal from "sweetalert2";
+import {useNavigate} from 'react-router-dom';
+
 
 const Login = () => {
+  const [CPF, setCPF] = useState("");
+  const [password, setPassword] = useState("");
+  const [msg, setMsg] = useState("");
+  const [errors, setErrors] = useState({
+    userCPF: null,
+    userPassword: null,
+  });
+
+  const formValidation = () => {
+    if (CPF === "") {
+      setErrors((errors) => {
+        const clone = { ...errors };
+        Swal.fire({
+          icon: 'warning',
+          title: 'Ops...',
+          text: 'Verifique se todos os campos foram preenchidos!'
+        })
+        return clone;
+      });
+      return false;
+    } else
+      setErrors((errors) => {
+        const clone = { ...errors };
+        clone.userCPF = null;
+        return clone;
+      });
+
+    if (password === "") {
+      console.log(1);
+      setErrors((errors) => {
+        const clone = { ...errors };
+        Swal.fire({
+          icon: 'warning',
+          title: 'Ops...',
+          text: 'Verifique se todos os campos foram preenchidos!'
+        })
+        return clone;
+      });
+      return false;
+    } else
+      setErrors((errors) => {
+        const clone = { ...errors };
+        clone.userPassword = null;
+        return clone;
+      });
+
+    setMsg("");
+    // alert(msg)
+    return true;
+  };
+  const navigate = useNavigate();
+  const Alert = (event) => {
+    event.preventDefault();
+    if (formValidation()) navigate('/home');
+
+   
+
+  
+  };
+
   return (
     <>
-      <nav className='nav-login'>
-        <img className='nav-logo' src={ Logo } alt='logotipo da DNC Store' />
+      <nav className="nav-login">
+        <img className="nav-logo" src={Logo} alt="logotipo da DNC Store" />
       </nav>
-      <section className='login-info'>
-        <h1 className='login-title'>Acesse com seu login ou cadastre-se!</h1>
-        <h2 className='login-subtitle'>você pode entrar com o seu CPF</h2>
+      <section className="login-info">
+        <h1 className="login-title">Acesse com seu login ou cadastre-se!</h1>
+        <h2 className="login-subtitle">você pode entrar com o seu CPF</h2>
 
-        <form className='login-form'>
-          <label className='label-form'>Digite seu CPF:</label>
-          <div className='input-form'>
-            <input className='input-field' type='text' name='cpf' id='cpf' placeholder='000.000.000-00' />
+        <form className="login-form" onSubmit={Alert}>
+          <label className="label-form">Digite seu CPF:</label>
+          <div className="input-form">
+            <input
+              className="input-field"
+              type="number"
+              name="cpf"
+              id="cpf"
+              placeholder="000.000.000-00"
+              value={CPF}
+              onChange={(e) => {
+                setCPF(e.target.value);
+              }}
+            />
+
+            {errors.userCPF && (
+              <span className="warning">{errors.userCPF}</span>
+            )}
           </div>
-          <label className='label-form'>Senha:</label>
-          <div className='input-form'>
-            <input className='input-field' type='password' name='password' id='password' placeholder='***************' />
+          <label className="label-form">Senha:</label>
+          <div className="input-form">
+            <input
+              className="input-field"
+              type="password"
+              name="password"
+              id="password"
+              placeholder="***************"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+            {errors.userPassword && (
+              <span className="warning">{errors.userPassword}</span>
+            )}
           </div>
-          <Link to={`/home`}><button className='btn-login'>Entrar</button></Link>
+          <button className='btn-login' type={"submit"}>Entrar</button>
         </form>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
