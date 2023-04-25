270eff0051394c22a2e81cd308db53d314a73ec2
netoeac <netoeac.dev@gmail.com>
2023-04-24 21:02:02 -0300

    fix image home error

--- src/mock/products.mock.js
+++ src/mock/products.mock.js
@@ -1,7 +1,7 @@
 export const PRODUCTS_MOCKS = [
     {
         id: 1,
-        imgPath: "produto_1.png",
+        imgPath: "product-1.jpg",
         imgPathDetail: "../../../product-1.jpg",
         title: "Console Xbox Series X 1TB SSD",
         infoProducts: "Apresentamos o Xbox Series X, nosso console mais rápido e poderoso de todos os tempos. Jogue milhares de títulos de quatro gerações de consoles- todos os jogos têm melhor aparência e são melhor executados no Xbox Series X.",
@@ -12,7 +12,7 @@ export const PRODUCTS_MOCKS = [
     
     {
         id: 2,
-        imgPath: "produto_2.png",
+        imgPath: "product-2.jpg",
         imgPathDetail: "../../../product-2.jpg",
         title: "Console Xbox Series S 512GB SSD",
         infoProducts: "Para você que é fã de games e está a procura de um novo console para uma nova experiência, precisa conhecer o Xbox Series S da Microsoft na cor branca. Lançado em 2020, ele tem SSD de 512GB para armazenar diversos jogos e 1 controle sem fio que deixa você livre para jogar. Use apenas com jogos digitais. O console não reproduz discos físicos. Inclui o download gratuito dos jogos Fortnite, Rocket League e Fall Guys. O conteúdo é entregue diretamente ao seu console durante a configuração e nenhum código é necessário.",
@@ -23,7 +23,7 @@ export const PRODUCTS_MOCKS = [
 
     {
         id: 3,
-        imgPath: "produto_3.png",
+        imgPath: "product-3.jpg",
         imgPathDetail: "../../../product-3.jpg",
         title: "Nintendo Switch com Jogo",
         infoProducts: "Nintendo Switch é o mais novo console de videogame doméstico da Nintendo. Além de proporcionar diversão em casa para um ou mais jogadores, o console Nintendo Switch pode ser transformando em um console portátil num piscar de olhos.",
@@ -34,7 +34,7 @@ export const PRODUCTS_MOCKS = [
 
     {
         id: 4,
-        imgPath: "produto_4.png",
+        imgPath: "product-4.jpg",
         imgPathDetail: "../../../product-4.jpg",
         title: "Jogo God of War Playstation",
         infoProducts: "Sua vingança contra os deuses do Olimpo agora é passado, em God of War, Kratos vive como um homem comum nas terras dos deuses Nórdicos, ensinando seu filho a sobreviver a todos os perigos que aparecerem por sua frente. Kratos está empenhado em passar todos seus conhecimentos ao seu filho, que está determinado a conquistar o respeito de seu pai. Finalmente o assassino de deuses tem a oportunidade de corrigir seus erros do passado.",
@@ -45,7 +45,7 @@ export const PRODUCTS_MOCKS = [
 
     {
         id: 5,
-        imgPath: "produto_5.png",
+        imgPath: "product-5.jpg",
         imgPathDetail:"../../../product-5.jpg",
         title: "Jogo Uncharted: The Nathan Drake Collection",
         infoProducts: "Para você que é fã de games e está a procura de um novo console para uma nova experiência, precisa conhecer o Xbox Series S da Microsoft na cor branca. Lançado em 2020, ele tem SSD de 512GB para armazenar diversos jogos e 1 controle sem fio que deixa você livre para jogar. Use apenas com jogos digitais. O console não reproduz discos físicos. Inclui o download gratuito dos jogos Fortnite, Rocket League e Fall Guys. O conteúdo é entregue diretamente ao seu console durante a configuração e nenhum código é necessário.",
@@ -56,7 +56,7 @@ export const PRODUCTS_MOCKS = [
 
     {
         id: 6,
-        imgPath: "produto_6.png",
+        imgPath: "product-6.jpg",
         imgPathDetail: "../../../product-6.jpg",
         title: "Jogo Gran Turismo",
         infoProducts: "Nesse jogo a experiência definitiva de corridas está de volta e melhor do que nunca, somente no PlayStation 4 Gran Turismo Sport é a primeira experiência de corrida do mundo recriada do zero para trazer competições internacionais online autorizadas pelo maior órgão controlador de corridas automobilísticas internacionais, a FIA (Federação Internacional de Automobilismo).",
